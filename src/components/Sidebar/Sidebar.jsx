74197663cd1cdff21f8440a99fc0344cf0d9d7dc
Navindu Madhuwantha <navindu088@gmail.com>
2025-10-19 01:49:09 +0530

    Done Entire order Section

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,68 @@
 import React from "react";
-import {FaTachometerAlt,FaClipboardList,FaUtensils,FaStore,FaCog,FaChartBar,FaTags,FaUsers,FaTruck,FaMoneyBillWave,FaBell,} from "react-icons/fa";
+import { useState } from "react";
+import {FaTachometerAlt,FaClipboardList,FaUtensils,FaStore,FaCog,FaChartBar,FaTags,FaUsers,FaTruck,FaMoneyBillWave,FaBell,FaChevronDown,FaChevronRight} from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+    
+    const location = useLocation();
+
+    const [openMenu, setOpenMenu] = useState({
+    orders: false,
+    reservations: false,
+    management: false,
+  });
+
+  const toggleMenu = (menu) => {
+    setOpenMenu((prev) => ({ ...prev, [menu]: !prev[menu] }));
+  };
+
+
     return (
         <aside className="sidebar">
          <ul className="menu">
-            <li><FaTachometerAlt /> Dashboard</li>
+
+            <li>
+                <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+                    <FaTachometerAlt /> Dashboard
+                </Link>
+            </li>
+
             <li><FaUtensils /> Take Order</li>
+
             <li><FaClipboardList /> Reservations</li>
-            <li><FaMoneyBillWave /> Orders</li>
+
+            <li onClick={() => toggleMenu("orders")} className="submenu-toggle">
+                <div className="menu-item-left">
+                    <FaMoneyBillWave /> <span>Orders</span>
+                </div>
+                <div className="menu-item-right">
+                    {openMenu.orders ? <FaChevronDown /> : <FaChevronRight />}
+                </div>
+            </li>
+
+            {openMenu.orders && (
+            <ul className="submenu">
+                <li>
+                <Link
+                    to="/ActiveOrders"
+                    className={location.pathname === "/ActiveOrders" ? "active" : ""}
+                >
+                    Active Orders
+                </Link>
+                </li>
+                <li>
+                <Link
+                    to="/AllOrders"
+                    className={location.pathname === "/AllOrders" ? "active" : ""}
+                >
+                    All Orders
+                </Link>
+                </li>
+            </ul>
+            )}
+
             <li><FaStore /> Website Builder</li>
             <li><FaCog /> Website & Store Management</li>
             <li><FaTags /> Menu & Products</li>
