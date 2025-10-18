d8e3d8674e61642262bf2a33d6c46ad6ad39c617
Navindu Madhuwantha <navindu088@gmail.com>
2025-10-18 16:05:30 +0530

    Add header and sidebar for menu & products
    Add header and sidebar for menu & products

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import logo from './logo.svg';
 //import './App.css';
 import Dashboard from "./pages/Dashboard/Dashboard";
+import MenuItemAddedForm from "./pages/Menu & Products/MenuItemAddedForm";
 
 function App() {
   return (
 
     <BrowserRouter>
+
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/add-menu-item" element={<MenuItemAddedForm />} />
       </Routes>
     </BrowserRouter>
     
