e6373d729451edd6a67c22e9148f7e4b7ad658ca
HarithRajapaksha <priyaharshanaharith@gmail.com>
2025-10-20 12:09:39 +0530

    Menu item things are done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 import ActiveOrders from "./pages/Orders/ActiveOrders";
 import AllOrders from "./pages/Orders/AllOrders";
 import MenuItemAddedForm from "./pages/Menu & Products/MenuItemAddedForm";
+import MenuManagement from "./pages/Menu & Products/MenuManegement"
+import Menu from "./pages/Menu & Products/Menu";
 
 
 function App() {
@@ -18,7 +20,9 @@ function App() {
         <Route path="/ActiveOrders" element={<ActiveOrders />} />
         <Route path="/AllOrders" element={<AllOrders />} />
         <Route path="/menu-item-added" element={<MenuItemAddedForm />} />
-    
+        <Route path="/menu-management" element={<MenuManagement />} />
+        <Route path="/menu" element={<Menu />} />
+
       </Routes>
     </BrowserRouter>
     
---
+++ src/pages/Menu & Products/Menu.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Menu.css';
+
+function Menu() {
+  return (
+    <div className="menu-container">
+      {/* Info bubble */}
+      <div className="info-bubble">
+        <span className="bubble-number">1</span>
+        <a href="#" className="how-it-works">How it works!</a>
+      </div>
+
+      {/* Title and Add Button */}
+      <div className="title-section">
+        <h1 className="menu-title">Standard Menu</h1>
+        <button className="add-button">+ Add Menu Item</button>
+      </div>
+
+      {/* Home Made Toggle */}
+      <div className="toggle-section">
+        <label className="toggle-label">home made</label>
+        <div className="toggle-switch on">
+          <div className="toggle-knob"></div>
+        </div>
+      </div>
+
+      {/* Search Bar */}
+      <input 
+        type="text" 
+        placeholder="Search Product Name, Description or Price" 
+        className="search-input" 
+      />
+
+      {/* Menu Item Card */}
+      <div className="menu-item-card">
+        <div className="item-content">
+          <div className="item-details">
+            <h3 className="item-name">Pizza ★ 1</h3>
+            <p className="item-price">Rs 200.00</p>
+          </div>
+          <div className="toggle-switch off">
+            <div className="toggle-knob"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
